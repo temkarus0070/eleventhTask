4d6737bc5f33ddcd68c18e90bd8f357d6b06ae80
artyom <pretkovartyom@gmail.com>
2021-09-25 17:14:44 +0300

    add servlet and split all app into modules

--- userInterface/src/servlets/HomeServlet.java
+++ userInterface/src/servlets/HomeServlet.java
@@ -1,7 +1,30 @@
 package servlets;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 public class HomeServlet extends HttpServlet {
+    private final String pagePath="WEB-INF/jsp/home.jsp";
+    @Override
+    public void init() throws ServletException {
+        super.init();
+    }
 
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setAttribute("my kek","kek");
+        RequestDispatcher requestDispatcher=req.getRequestDispatcher(pagePath);
+        requestDispatcher.forward(req,resp);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setAttribute("my kek","BIG kek");
+        RequestDispatcher requestDispatcher=req.getRequestDispatcher(pagePath);
+        requestDispatcher.forward(req,resp);
+    }
 }
---
+++ userInterface/webapp/WEB-INF/jsp/home.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: artyom
+  Date: 11.09.2021
+  Time: 8:15
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<% String obj=(String) request.getAttribute("kek"); %>
+
+</body>
+</html>
