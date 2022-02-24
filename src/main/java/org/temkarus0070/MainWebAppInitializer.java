a3f124fead11fed40d5a405e971bcf444596e20e
temkarus0070 <pretkovartyom@gmail.com>
2022-02-24 18:36:21 +0300

    UPDATE

---
+++ src/main/java/org/temkarus0070/MainWebAppInitializer.java
@@ -0,0 +1,28 @@
+package org.temkarus0070;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.context.support.GenericWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+public class MainWebAppInitializer implements WebApplicationInitializer {
+    @Override
+    public void onStartup(final ServletContext sc) throws ServletException {
+
+        AnnotationConfigWebApplicationContext root =
+                new AnnotationConfigWebApplicationContext();
+
+        root.scan("org.temkarus0070");
+        sc.addListener(new ContextLoaderListener(root));
+
+        ServletRegistration.Dynamic appServlet =
+                sc.addServlet("mvc", new DispatcherServlet(new GenericWebApplicationContext()));
+        appServlet.setLoadOnStartup(1);
+        appServlet.addMapping("/");
+    }
+}
